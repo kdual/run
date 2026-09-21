9c48091890683ab6ead795c0bd31689538a957ae
kdual <jonghwan@tukorea.ac.kr>
2026-09-21 23:21:38 +0900

    Improve mobile hourly chart: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='runwise-static-v12';
-const STATIC=['./','./index.html','./styles.css?v=11','./responsive.css?v=11','./app.js?v=12','./weather.js?v=11','./air-quality.js?v=12','./running-score.js?v=11','./running-coach.js?v=11','./pace-calculator.js','./charts.js?v=11','./storage.js?v=11','./utils.js','./running-score-config.js?v=11','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='runwise-static-v13';
+const STATIC=['./','./index.html','./styles.css?v=12','./responsive.css?v=12','./app.js?v=13','./weather.js?v=11','./air-quality.js?v=12','./running-score.js?v=11','./running-coach.js?v=11','./pace-calculator.js','./charts.js?v=12','./storage.js?v=11','./utils.js','./running-score-config.js?v=11','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
