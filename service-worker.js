876f8a0291079372f970b1e8e6ea5ea4aa756ace
kdual <jonghwan@tukorea.ac.kr>
2026-09-26 14:27:27 +0900

    Balance three daily cards to fill available row height

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='runwise-static-v26';
-const STATIC=['./','./index.html','./styles.css?v=26','./responsive.css?v=20','./app.js?v=25','./weather.js?v=19','./air-quality.js?v=19','./running-score.js?v=20','./running-coach.js?v=14','./pace-calculator.js','./charts.js?v=12','./storage.js?v=19','./solar-times.js?v=21','./utils.js','./running-score-config.js?v=14','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='runwise-static-v27';
+const STATIC=['./','./index.html','./styles.css?v=27','./responsive.css?v=20','./app.js?v=25','./weather.js?v=19','./air-quality.js?v=19','./running-score.js?v=20','./running-coach.js?v=14','./pace-calculator.js','./charts.js?v=12','./storage.js?v=19','./solar-times.js?v=21','./utils.js','./running-score-config.js?v=14','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
