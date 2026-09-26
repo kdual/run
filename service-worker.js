264084068074b4e1d9decb5e1c08a51c53782ebd
kdual <jonghwan@tukorea.ac.kr>
2026-09-26 18:38:58 +0900

    Make pace and speed input easier on iPhone

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='runwise-static-v29';
-const STATIC=['./','./index.html','./styles.css?v=28','./responsive.css?v=20','./app.js?v=29','./weather.js?v=19','./air-quality.js?v=19','./running-score.js?v=20','./running-coach.js?v=29','./pace-calculator.js','./charts.js?v=12','./storage.js?v=19','./solar-times.js?v=21','./utils.js','./running-score-config.js?v=14','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='runwise-static-v30';
+const STATIC=['./','./index.html','./styles.css?v=28','./responsive.css?v=20','./app.js?v=30','./weather.js?v=19','./air-quality.js?v=19','./running-score.js?v=20','./running-coach.js?v=29','./pace-calculator.js?v=30','./charts.js?v=12','./storage.js?v=19','./solar-times.js?v=21','./utils.js','./running-score-config.js?v=14','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
